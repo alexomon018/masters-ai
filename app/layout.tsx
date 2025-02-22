1b7fdfe349019379ee653ac1e670df07683be5c9
Aleksa <aleksa.mitic5859@gmail.com>
2025-02-22 14:15:34 +0100

    small fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@utils";
-import { ThemeProvider } from "@/providers/themeProvider";
 
 const fontSans = FontSans({
 	subsets: ["latin"],
