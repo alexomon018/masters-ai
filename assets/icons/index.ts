74ff4c3e340f08bfe46c2f2b3e8d14fe3395289e
Aleksa <>
2025-03-13 15:43:14 +0100

    fix google icon

--- assets/icons/index.ts
+++ assets/icons/index.ts
@@ -2,12 +2,14 @@ import openaiLogo from "./openai.svg";
 import geminiLogo from "./gemini.svg";
 import groqLogo from "./grok.svg";
 import anthropicLogo from "./anthropic.svg";
+import googleLogo from "./google.svg";
 
 const Icons = {
 	openai: openaiLogo,
 	gemini: geminiLogo,
 	groq: groqLogo,
-	anthropic: anthropicLogo
+	anthropic: anthropicLogo,
+	google: googleLogo
 };
 
 export default Icons;
