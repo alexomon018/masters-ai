18fb2af42926144e09125d109409abbc38dbea35
Aleksa <aleksa.mitic5859@gmail.com>
2025-03-19 20:10:47 +0100

    cleanup and fixes

--- ai/llm.ts
+++ ai/llm.ts
@@ -1,9 +1,7 @@
-import type { Msg } from "@dexaai/dexter";
-import { ChatModel } from "@dexaai/dexter";
 import { zodResponseFormat } from "openai/helpers/zod";
 import { z } from "zod";
-import { openai } from "./ai";
-import { summarizePrompt, nameThreadPrompt } from "./systemPrompt";
+import { openai, AIMessage } from "./ai";
+import { nameThreadPrompt } from "./systemPrompt";
 
 export const runApprovalCheck = async (userMessage: string) => {
 	const response = await openai.beta.chat.completions.parse({
@@ -28,28 +26,7 @@ export const runApprovalCheck = async (userMessage: string) => {
 	return response.choices[0].message.parsed?.approved;
 };
 
-export const summarizeMessages = async (messages: Msg[]) => {
-	const chatModel = new ChatModel({
-		params: {
-			model: "gpt-4o-mini",
-			temperature: 0.1
-		}
-	});
-
-	const response = await chatModel.run({
-		messages: [
-			{
-				role: "system",
-				content: summarizePrompt
-			},
-			...messages
-		]
-	});
-
-	return response.choices[0].message.content;
-};
-
-export const runLLM = async (messages: Msg[]) => {
+export const runLLM = async (messages: AIMessage[]) => {
 	const response = await openai.chat.completions.create({
 		model: "gpt-4o",
 		messages: [{ role: "system", content: nameThreadPrompt }, ...messages]
